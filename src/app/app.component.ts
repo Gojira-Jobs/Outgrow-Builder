d9093b013c26ba0bbb16f79d41bcec8c78d0852a
Ajit <jainajit194@gmail.com>
2017-02-01 17:32:13 +0530

    Initial Structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,29 @@
-import {Component} from '@angular/core';
-
+import {Component,ElementRef,OnInit,AfterViewInit} from '@angular/core';
+declare var jQuery:any;
 @Component({
   selector: 'app-root',
-  template: '<h1>hello builder</h1>'
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit,AfterViewInit{
+  constructor(private ele:ElementRef){}
+  ngOnInit():any{
+    console.log(jQuery(this.ele.nativeElement));
+    jQuery(this.ele.nativeElement).find('#create_section').on('click',function(){
+
+      console.log(jQuery(this.ele));
+    // jQuery(this.ele.nativeElement).find("#content-area").append(" <b>Appended text</b>.")
+    });
+
+  }
+  ngAfterViewInit(){
+     console.log(jQuery(this.ele.nativeElement));
+    jQuery(this.ele.nativeElement).find('#create_section').on('click',function(){
+      alert("ssjsj");
+     // console.log(jQuery(this.ele));
+    // jQuery(this.ele.nativeElement).find("#content-area").append(" <b>Appended text</b>.")
+    // });
+    });
+}
+
+
 }
