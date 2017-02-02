fd3e27ae642ecc054bf2b6741fca7ad9034da90e
Ajit <jainajit194@gmail.com>
2017-02-02 17:20:15 +0530

    Selection area

--- src/app/builder/components/pages/lead-generation/lead-generation.component.ts
+++ src/app/builder/components/pages/lead-generation/lead-generation.component.ts
@@ -1,14 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit,AfterViewInit} from '@angular/core';
+declare var jQuery:any;
 @Component({
   selector: 'lead-generation',
   templateUrl: './lead-generation.component.html',
 })
-export class LeadGenerationComponent implements OnInit {
-
+export class LeadGenerationComponent implements OnInit,AfterViewInit {
+ngAfterViewInit(){
+}
   constructor() { }
 
   ngOnInit() {
   }
-
+  checkforSelection(event){
+    if(window.getSelection().toString()){
+      
+      console.log("jdjd"+window.getSelection().getRangeAt(0).startOffset+window.getSelection);
+     console.log( jQuery(event.target).text().slice(window.getSelection().getRangeAt(0).startOffset,
+    window.getSelection().getRangeAt(0).endOffset));
+    var startIndex = window.getSelection().getRangeAt(0).startOffset;
+    var endIndex = window.getSelection().getRangeAt(0).endOffset;
+    var slicedText = jQuery(event.target).text().slice(startIndex, endIndex);
+    var text=window.getSelection().toString();
+    jQuery(event.target).html(jQuery(event.target).text().replace(slicedText,'<span id='+window.getSelection()+'>' + slicedText + '</span>'));
+     
+     console.log(text);
+      jQuery("#"+text).toolbar({
+        content: '#toolbar-options',
+        event:'select'
+      });
+    }
+  // var start = 0, end = 0;
+  //   var sel, range, priorRange;
+  //   range = window.getSelection().getRangeAt(0);
+  //       priorRange = range.cloneRange();
+  //       priorRange.selectNodeContents(event.target);
+  //       priorRange.setEnd(range.startContainer, range.startOffset);
+  //       start = priorRange.toString().length;
+  //       end = start + range.toString().length;
+  //       console.log(start);
+      
+  }
 }
