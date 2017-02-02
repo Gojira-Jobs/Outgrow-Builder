74015634186443d7380f60ae0149ee38296a3373
rahul kumar <rahulkumar2048@gmail.com>
2017-02-02 15:29:47 +0530

    content can be edited inline now

--- src/app/builder/components/pages/lead-generation/lead-generation.component.ts
+++ src/app/builder/components/pages/lead-generation/lead-generation.component.ts
@@ -1,14 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
 
 @Component({
-  selector: 'lead-generation',
-  templateUrl: './lead-generation.component.html',
+    selector: 'lead-generation',
+    templateUrl: './lead-generation.component.html',
 })
 export class LeadGenerationComponent implements OnInit {
 
-  constructor() { }
+    mainHeading: string;
+    subHeading: string;
+    nameInputBox: string;
+    emailInputBox: string;
+    button: string;
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
+
+    private initializeViewContent() {
+        this.mainHeading = "Calculate the risk of you getting a heart disease.";
+        this.subHeading = "Heart problems are at an all time high. See if your lifestyle makes you susceptible.";
+        this.nameInputBox = "John Doe"
+        this.emailInputBox = "John@outgrow.co";
+        this.button = "Estimate Costs";
+    }
+
+    ngOnInit() {
+        this.initializeViewContent();
+    }
 
 }
