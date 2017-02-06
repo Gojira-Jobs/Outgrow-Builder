e4c27260639607ebd6c21e1b98fa9b16f7a1a3ce
Ajit <jainajit194@gmail.com>
2017-02-06 17:12:38 +0530

    directive created for editing

--- src/app/builder/toolbar.component.ts
+++ src/app/builder/toolbar.component.ts
@@ -19,10 +19,18 @@ export class ToolbarComponent implements OnInit {
         if (type == 'italic') tag = 'i';
         if (type == 'underline') tag = 'u';
         var appended = document.createElement(tag);
+        var sel=window.getSelection();
         appended.textContent = window.getSelection().toString();
         console.log(window.getSelection().toString());
         var range = window.getSelection().getRangeAt(0);
         range.deleteContents();
         range.insertNode(appended);
+        // var range2=document.createRange();
+        // range2.setStart(window.getSelection().getRangeAt(0).commonAncestorContainer.parentNode.childNodes[0],window.getSelection().getRangeAt(0).startOffset-1);
+        // range2.collapse(true);
+        // sel.removeAllRanges();
+        // sel.addRange(range2);
+       // window.getSelection().getRangeAt(0).commonAncestorContainer.parentNode.focus()
+        
     }
 }
