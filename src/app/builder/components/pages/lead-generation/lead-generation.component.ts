cd9596b070e1815ecef313ad7ed969a86502da39
rahul kumar <rahulkumar2048@gmail.com>
2017-02-08 14:51:46 +0530

    now space can be applied in button inside lead generation

--- src/app/builder/components/pages/lead-generation/lead-generation.component.ts
+++ src/app/builder/components/pages/lead-generation/lead-generation.component.ts
@@ -1,7 +1,9 @@
 import {Script} from "../../../services/script.service";
 import {Page, SUBMIT_BUTTON, INPUT_NAME, INPUT_EMAIL, SUB_HEADING, MAIN_HEADING} from "../../../models/PageModel";
 import {SavePage, sectionLeadGeneration} from "../../../services/savePage.service";
-import {Component, OnInit} from "@angular/core";
+import {Component, OnInit, ElementRef} from "@angular/core";
+import {DomEventsPlugin} from "@angular/platform-browser/src/dom/events/dom_events";
+import {noop} from "@angular/core/src/linker/view_utils";
 declare var jQuery: any
 declare var filepicker: any;
 
@@ -49,13 +51,18 @@ export class LeadGenerationComponent implements OnInit {
         this.page.control[SUBMIT_BUTTON] = "Estimate Costs";
     }
 
+    change(event) {
+        console.log('kamelot', event);
+    }
+
     setMainHeading(event) {
-        this.page.control[MAIN_HEADING] = event.target.textContent;
+        console.log('innerhtml', event.target.innerHTML);
+        this.page.control[MAIN_HEADING] = event.target.innerHTML;
         this.savePageService.notifyPageChanges(this.page);
     }
 
     setSubHeading(event) {
-        this.page.control[SUB_HEADING] = event.target.textContent;
+        this.page.control[SUB_HEADING] = event.target.innerHTML;
         this.savePageService.notifyPageChanges(this.page);
     }
 
@@ -70,8 +77,51 @@ export class LeadGenerationComponent implements OnInit {
     }
 
     setSubmitButton(event) {
-        this.page.control[SUBMIT_BUTTON] = event.target.textContent;
+        this.page.control[SUBMIT_BUTTON] = event.target.innerHTML;
         this.savePageService.notifyPageChanges(this.page);
+    }
+
+    insertSpace(event) {
+        if (event.code == "Space") {
+            let index = LeadGenerationComponent.getCaretCharacterOffsetWithin(event.target);
+            let content = event.target.textContent;
+            event.target.innerHTML = content.slice(0, index) + "&nbsp;" + content.slice(index);
+
+            let node = document.querySelector("button");
+            node.focus();
+            let textNode = node.firstChild;
+
+            let range = document.createRange();
+            range.setStart(textNode, index + 1);
+            range.setEnd(textNode, index + 1);
+            let sel = window.getSelection();
+            sel.removeAllRanges();
+            sel.addRange(range);
+        }
+    }
+
+    static getCaretCharacterOffsetWithin(element) {
+        let caretOffset = 0;
+        let doc = element.ownerDocument || element.document;
+        let win = doc.defaultView || doc.parentWindow;
+        let sel;
+        if (typeof win.getSelection != "undefined") {
+            sel = win.getSelection();
+            if (sel.rangeCount > 0) {
+                let range = win.getSelection().getRangeAt(0);
+                let preCaretRange = range.cloneRange();
+                preCaretRange.selectNodeContents(element);
+                preCaretRange.setEnd(range.endContainer, range.endOffset);
+                caretOffset = preCaretRange.toString().length;
+            }
+        } else if ((sel = doc.selection) && sel.type != "Control") {
+            var textRange = sel.createRange();
+            var preCaretTextRange = doc.body.createTextRange();
+            preCaretTextRange.moveToElementText(element);
+            preCaretTextRange.setEndPoint("EndToEnd", textRange);
+            caretOffset = preCaretTextRange.text.length;
+        }
+        return caretOffset;
     }
 
     uploadImage(control: any) {
