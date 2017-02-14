d97e32d55cc8b336cb0e025618229bf93ae78118
Ajit <jainajit194@gmail.com>
2017-02-14 12:00:29 +0530

    Control Rendering

--- src/app/builder/controls/header/header.component.ts
+++ src/app/builder/controls/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit,Input } from '@angular/core';
-
+import {FroalaOptions} from '../froala-options';
 @Component({
   selector: 'og-header',
   template: `
@@ -11,16 +11,10 @@ import { Component, OnInit,Input } from '@angular/core';
   `,
   styles: []
 })
-export class Header implements OnInit {
+export class Header extends FroalaOptions implements OnInit {
   @Input() data:any;
   mainheading:any="";
-public options: Object = {
-        placeholderText: 'Edit Your Content Here!',
-        toolbarInline: true,
-        charCounterCount: false,
-        toolbarButtons: ['bold', 'italic', 'underline', 'color', 'html', 'clearFormatting','paragraphFormat'],
-    };
-  constructor() { }
+  constructor() { super(); }
 
   ngOnInit() {
   }
--- src/app/builder/services/DefaultJSON.service.ts
+++ src/app/builder/services/DefaultJSON.service.ts
@@ -9,7 +9,7 @@ export class DefaultJSON {
     getJson(template: string): App {
 
         let app = new App("outgrow builder");
-        let landingPage = new Page("Landing",'../../assets/bg-new.jpg');
+        let landingPage = new Page("Landing",'https://res.cloudinary.com/ajitjain/image/upload/v1487051060/bg-new_vk8qjg.jpg');
 
         let sectionHead = new Section('Logo Heading');
         let logoItem = new Item('logo', 'https://www.filestackapi.com/api/file/jJx4Amp9SjiYVGsOA1uK');
