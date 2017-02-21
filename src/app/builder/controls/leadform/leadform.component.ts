c0ceab6d783fa90cdfefea46afa7444320c686a0
rahul kumar <rahulkumar2048@gmail.com>
2017-02-21 11:07:18 +0530

    now caret position is set on placeholder

--- src/app/builder/controls/leadform/leadform.component.ts
+++ src/app/builder/controls/leadform/leadform.component.ts
@@ -1,47 +1,72 @@
-import { Component, OnInit,Input,Output,EventEmitter,OnChanges,ViewEncapsulation} from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import {Helper} from '../helpers/helper'
+import {Component, OnInit, Input, ViewEncapsulation} from "@angular/core";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {Helper} from "../helpers/helper";
 @Component({
-  selector: 'leadform',
-  templateUrl: './leadform.component.html',
-   encapsulation:ViewEncapsulation.None
+    selector: 'leadform',
+    templateUrl: './leadform.component.html',
+    encapsulation: ViewEncapsulation.None
 })
-export class LeadformComponent extends Helper implements OnInit,OnChanges {
+export class LeadformComponent extends Helper implements OnInit {
 
-@Input() data;
-@Input() page;
-leadform:FormGroup;
+    @Input() data;
+    @Input() page;
+    leadform: FormGroup;
 
-ngOnChanges(){
-  
-}
-  constructor(private formBuilder:FormBuilder) {
-    super();
-  }
-
-  ngOnInit() {
-    if(this.data!=undefined){
-        console.log(this.data);
-        let obj:any={};
-        this.data.fields.forEach((item)=>{
-            let validations=new Array();
-            validations.push(Validators.required);
-            if(item.name=='Email') validations.push(Validators.pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/));
-            obj[item.name]=new FormControl('',
-            Validators.compose(validations));
-        })
-        this.leadform=new FormGroup(obj);
-        console.log(this.leadform.value);
-        
+    constructor(private formBuilder: FormBuilder) {
+        super();
     }
-  }
-  setChanges(index:number,newValue:string){
-    this.data.fields[index].placeholder=newValue;
-    this.leadform.valueChanges.debounceTime(200).subscribe((value)=>{
-          //if(this.leadform.valid){
-            this.emitChanges('done');
-         // }
-    })
-  }
 
+    ngOnInit() {
+        if (this.data != undefined) {
+            console.log(this.data);
+            let obj: any = {};
+            this.data.fields.forEach((item) => {
+                let validations = new Array();
+                validations.push(Validators.required);
+                if (item.name == 'Email') validations.push(Validators
+                    .pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/));
+                obj[item.name] = new FormControl('',
+                    Validators.compose(validations));
+            })
+            this.leadform = new FormGroup(obj);
+            console.log(this.leadform.value);
+
+        }
+    }
+
+    setChanges(index: number, newValue: string) {
+        this.data.fields[index].placeholder = newValue;
+        this.leadform.valueChanges.debounceTime(200).subscribe((value) => {
+            //if(this.leadform.valid){
+            this.emitChanges('done');
+            // }
+        });
+    }
+
+    updateCaretAndPlaceholder(leadformInput, data, i) {
+        leadformInput.value = data.fields[i].placeholder;
+        let pos = this.getCaretPos(leadformInput);
+        this.setSelectionRange(leadformInput, pos, pos);
+    }
+
+    getCaretPos(input): any {
+        let caretPos;
+        if (input.selectionStart || input.selectionStart == '0') {
+            caretPos = input.selectionStart;
+        }
+        return caretPos;
+    }
+
+    setSelectionRange(input, selectionStart, selectionEnd) {
+        if (input.setSelectionRange) {
+            input.focus();
+            input.setSelectionRange(selectionStart, selectionEnd);
+        } else if (input.createTextRange) {
+            var range = input.createTextRange();
+            range.collapse(true);
+            range.moveEnd('character', selectionEnd);
+            range.moveStart('character', selectionStart);
+            range.select();
+        }
+    }
 }
