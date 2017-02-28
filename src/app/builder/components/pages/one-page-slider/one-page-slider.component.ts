82450ffe3581651cd0d1a67def709e74c35098ac
rahul kumar <rahulkumar2048@gmail.com>
2017-02-28 17:15:10 +0530

    fix some bugs again

--- src/app/builder/components/pages/one-page-slider/one-page-slider.component.ts
+++ src/app/builder/components/pages/one-page-slider/one-page-slider.component.ts
@@ -1,22 +1,19 @@
-import {Component, OnInit, Input, OnChanges} from "@angular/core";
+import {Component, OnInit, Input} from "@angular/core";
 import {SavePage} from "../../../services/savePage.service";
 import {App} from "../../../models/App";
 @Component({
     selector: 'one-page-slider',
     templateUrl: './one-page-slider.component.html',
-    styles:[`.landing-page {width: 100vw;display: table-cell;vertical-align: middle;height: 72vh;text-align:center;}
+    styles: [`.landing-page {width: 100vw;display: table-cell;vertical-align: middle;height: 72vh;text-align:center;}
           .pageSetter{margin-bottom:5px}
           `]
 })
-export class OnePageSliderComponent implements OnInit,OnChanges {
-    ngOnChanges() {
-    }
+export class OnePageSliderComponent implements OnInit {
 
     @Input()
     jsonTemplate: App;
 
     constructor(private savePageService: SavePage) {
-
     }
 
     ngOnInit() {
